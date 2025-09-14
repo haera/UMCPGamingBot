e9cbf332687bae4934094210b790a398e9e28ca5
haera <25065660+haera@users.noreply.github.com>
2025-09-14 09:24:15 -0400

    async func fix 6?

--- plugins/umcp/umcp.py
+++ plugins/umcp/umcp.py
@@ -25,10 +25,15 @@ class UMCPBot(commands.Cog):
 
         self.role_msgs: Dict[int, discord.Message] = {}
         #self.role_assign_cooldown = SpamLimit(commands.Cooldown(rate=30, per=120, bucket_type=commands.BucketType.user))
-
-        self.umcp_server: discord.Guild = None
-        self.role_channel: discord.TextChannel = None
-        self.streamer_role: discord.Role = None
+        #self.umcp_server: discord.Guild = self.client.get_guild(config["guild_id"])
+        #self.role_channel: discord.TextChannel = self.umcp_server.get_channel(config["role_channel_id"])
+        #self.streamer_role: discord.Role = self.umcp_server.get_role(config["streamer_role_id"])
+        self.umcp_server: discord.Guild = self.client.get_guild(config["guild_id"])
+        self.role_channel: discord.TextChannel = self.umcp_server.get_channel(config["role_channel_id"])
+        self.streamer_role: discord.Role = self.umcp_server.get_role(config["streamer_role_id"])
+        
+        self.role_channel_cleanup.start()
+        await self.check_streaming_role()
 
     def cog_unload(self):
         self.role_channel_cleanup.cancel()
