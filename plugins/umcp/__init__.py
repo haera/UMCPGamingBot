15e993d9c2f8badc6fbf6a50f0bbabc4ea347927
haera <25065660+haera@users.noreply.github.com>
2025-09-14 09:24:15 -0400

    async func fix, awaiting add_cog call

--- plugins/umcp/__init__.py
+++ plugins/umcp/__init__.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 config: dict = {}
 
 
-def init(bot: commands.Bot, cfg: dict):
+async def init(bot: commands.Bot, cfg: dict):
     config.update(cfg[__name__])
 
     from .umcp import UMCPBot
-    bot.add_cog(UMCPBot(bot))
+    await bot.add_cog(UMCPBot(bot))
